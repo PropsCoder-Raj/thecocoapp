c7be77eef60fd42d5970880ab1dcb6908444ac62
RohitSIT <rohit@spareit.co.in>
2024-08-18 19:45:51 +0530

    Updated Frontend Build.

--- frontend/src/views/pages/dashboard/TakeQuiz.js
+++ frontend/src/views/pages/dashboard/TakeQuiz.js
@@ -40,10 +40,10 @@ const style = {
     height: "calc(100vh - 200px)",
   },
   buttonHandle: {
-    
-    position:"fixed",
-    width:"-webkit-fill-available",
-    bottom:"0"
+
+    position: "fixed",
+    width: "-webkit-fill-available",
+    bottom: "0"
   },
 };
 const MainBox = styled(Box)(({ theme }) => ({
@@ -60,19 +60,19 @@ const InnerBox = styled(Box)(({ theme }) => ({
   "@media(max-width:100px)": {
     padding: "30px",
     borderTop: "1px solid #E5E5E5",
-},
-  "@media(max-width:767px)": { padding: "20px", border:"none" },
-  background:"#fff"
+  },
+  "@media(max-width:767px)": { padding: "20px", border: "none" },
+  background: "#fff"
 }));
 
 const TakeImg = styled("img")(({ theme }) => ({
   maxWidth: "200px",
-  "@media(max-width:767px)": {width:"100%"},
+  "@media(max-width:767px)": { width: "100%" },
 }));
 function TakeQuiz() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [ closeAdd, setCloseAdd]= useState(true);
+  const [closeAdd, setCloseAdd] = useState(true);
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
@@ -93,74 +93,86 @@ function TakeQuiz() {
           </Grid>
           <Grid item xs={12}>
             <Box sx={style.CombineBox}>
-              <Box sx={{position:"relative",
+              <Box sx={{
+                position: "relative",
                 display: "grid",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "354px",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "354px",
               }}>
-              <TakeImg src="images/Coco-Idle_Without_Talking.gif" alt="" />
-              <Box sx={{ borderRadius: "8px", border:"1px solid #D8D8D8", padding:"25px", width:"354px", position:"absolute", bottom:{
-                md:"-90px",
-                sm:"-90px",
-                xs:"-50px"
-              }, background:"#fff"}}>
-                <Typography variant="h4">Answer 3 questions and know do you remember what you read!</Typography>
-              </Box></Box>
+                <TakeImg src="images/Coco-Idle_Without_Talking.gif" alt="" />
+                <Box sx={{
+                  borderRadius: "8px", border: "1px solid #D8D8D8", padding: "25px", width: "354px", position: "absolute", bottom: {
+                    md: "-90px",
+                    sm: "-90px",
+                    xs: "-50px"
+                  }, background: "#fff"
+                }}>
+                  <Typography variant="h4">Answer 3 questions and know do you remember what you read!</Typography>
+                </Box></Box>
             </Box>
           </Grid>
         </Grid>
       </Container>
       <InnerBox sx={style.buttonHandle}>
         <Container>
-          <Grid  spacing={4}>
-           
+          <Grid spacing={4}>
+
             <Grid item xs={12}>
               <Box >
                 <Box sx={{
                   display: "flex",
                   justifyContent: "end",
                   gap: "8px",
-              }}>
-              <Button variant="contained" onClick={()=>{
-                  navigate("/questions", {
-                    state: {
-                      module_id: location?.state?.level_id,
-                      level_id: location?.state?.module_id,
-                      nextQuestionNo:1,
-                    },
-                  });
-                }}
-                sx={{
-                  width:{
-                    md:"155px",
-                    sm: "-webkit-fill-available",
-                    xs:"-webkit-fill-available"
-                  }
-                }}
-                >Take Quiz</Button>
+                }}>
+                  <Button variant="contained" onClick={() => {
+                    navigate("/questions", {
+                      state: {
+                        module_id: location?.state?.level_id,
+                        level_id: location?.state?.module_id,
+                        nextQuestionNo: 1,
+                      },
+                    });
+                  }}
+                    sx={{
+                      width: {
+                        md: "155px",
+                        sm: "-webkit-fill-available",
+                        xs: "-webkit-fill-available"
+                      }
+                    }}
+                  >Take Quiz</Button>
                 </Box>
-               
+
               </Box>
             </Grid>
           </Grid>
         </Container>
       </InnerBox>
       {isMobile && closeAdd &&
-        <Box sx={{ position: "fixed", top: "0", width: "100%", height: "100vh", zIndex: "1", background:"rgb(255, 255, 255, 0.75)" }}>
-        <Box sx={{padding:"20px", display:"flex", justifyContent:"end"}}>
-          <IoMdClose
+        <Box sx={{ position: "fixed", top: "0", width: "100%", height: "100vh", zIndex: "1", background: "rgb(255, 255, 255, 0.75)" }}>
+          <Box sx={{ padding: "20px", display: "flex", justifyContent: "end" }}>
+            <IoMdClose
               color="#000"
-            onClick={() => {
-              setCloseAdd(false)
-            }}
-            cursor={"pointer"}
-            size={"30px"}
-          />
+              onClick={() => {
+                setCloseAdd(false)
+              }}
+              cursor={"pointer"}
+              size={"30px"}
+            />
+          </Box>
+          <Box sx={{ height: "100vh" }}>
+            <Box sx={{
+              height: '100vh',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+              <AdSense className="adsence-block" />
+            </Box>
+          </Box>
         </Box>
-        <AdSense className="adsence-block" />
-        </Box>
-        }
+      }
     </MainBox>
   );
 }
